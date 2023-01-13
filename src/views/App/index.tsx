784858e8f5e709639ab1fde1c352c1c9f20d68b3
Roman <roman.shemelin31@gmail.com>
2023-01-13 19:19:41 +0300

    create input component

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,6 +1,7 @@
-import  { FC, useEffect } from "react";
+import { FC } from "react";
 import styles from "./index.module.scss";
 import { useToDoStore } from "../../data/stores/useTodoStore";
+import InputTask from "../components/InputTask";
 
 export const App: FC = () => {
   const [tasks, createTask, removeTask, updateTask] = useToDoStore((state) => [
@@ -10,11 +11,18 @@ export const App: FC = () => {
     state.updateTask,
   ]);
 
+  const onAdd = (title) => {
+    if (title) {
+      createTask(title);
+    }
+  };
 
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
-      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}>
+        <InputTask onAdd={onAdd} />
+      </section>
       <section className={styles.articleSection}></section>
     </article>
   );
